a442b216088424d7686c6c2939f6f2c03083690d
Rodrigo Franco <caffeine@gmail.com>
2011-12-14 10:56:34 -0500

    0.4.0

---
+++ Rakefile
@@ -0,0 +1,53 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+require 'rake'
+
+require 'jeweler'
+Jeweler::Tasks.new do |gem|
+  # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
+  gem.name = "anotherinbox"
+  gem.homepage = "http://github.com/caffo/anotherinbox"
+  gem.license = "MIT"
+  gem.summary = "gmail based otherinbox defender alternative"
+  gem.description = "The original Otherinbox Defender is no more. Sadly, the newest version is subpar and do not meet my needs. This project attempts to reproduce the core functionalities of OIB Defender using a GMail account."
+  gem.email = "caffeine@gmail.com"
+  gem.authors = ["rodrigo franco (caffo)"]
+  # Include your dependencies below. Runtime dependencies are required when using your gem,
+  # and development dependencies are only needed for development (ie running rake tasks, tests, etc)
+    gem.add_runtime_dependency 'gmail', '>= 0.4.0'
+  #  gem.add_development_dependency 'rspec', '> 1.2.3'
+end
+Jeweler::RubygemsDotOrgTasks.new
+
+require 'rake/testtask'
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
+end
+
+require 'rcov/rcovtask'
+Rcov::RcovTask.new do |test|
+  test.libs << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
+end
+
+task :default => :test
+
+require 'rake/rdoctask'
+Rake::RDocTask.new do |rdoc|
+  version = File.exist?('VERSION') ? File.read('VERSION') : ""
+
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title = "anotherinbox #{version}"
+  rdoc.rdoc_files.include('README*')
+  rdoc.rdoc_files.include('lib/**/*.rb')
+end
---
+++ lib/anotherinbox.rb
@@ -0,0 +1,129 @@
+require 'rubygems'
+require 'gmail'
+require 'logger'
+require 'date'
+require 'digest/sha1'
+require 'aws/s3'
+
+class Anotherinbox < Object
+  def self.process_inbox!(email_address, password, target_email)
+    log = Logger.new(STDOUT)
+    log.info("Account: #{email_address}")
+    Gmail.new(email_address, password) do |gmail|
+      gmail.inbox.emails(:unread).each do |email|
+        mailbox = email[:to][0].mailbox.downcase rescue "unknown"
+        gmail.labels.new(mailbox)
+        if gmail.mailbox(mailbox).count == 0
+          log.info("First email for [#{gmail.mailbox(mailbox)}], forwarding..")
+          fwd                 = gmail.message
+          fwd.to              = target_email
+          fwd.subject         = "New Sender: #{email.subject}"
+          fwd.content_type    = "text/html"
+          body                = email.parts.last.body.to_s rescue nil
+          body              ||= email.body.to_s
+          fwd.body            = "-------------------------- ANOTHERINBOX ----------------------------------<br/>
+          You have received a message from : #{mailbox}<br/>
+          We have created a new label : #{mailbox}<br/>
+          -------------------------- ANOTHERINBOX ----------------------------------<br/><br/>" + body
+          fwd.deliver!
+        end
+        email.label(mailbox)
+        email.unread!
+        email.archive!
+        log.info("\tProcessing #{email[:subject]}")
+      end
+    end
+    log.info("Process finished")
+  end
+
+  def self.daily_digest(email_address, password, target_email, configs)
+
+    def self.email_template(new_messages, account, configs, email_address)
+      # we all know, inline styles sucks. sadly, it's the
+      # only way to get them into gmail.
+      response = ""
+      response += "<h1 style='margin-left: 40px; color: #000000;'>Daily Digest for #{(Date.today - 1)}</h1>"
+      response += "<h3 style='color: #aaaaaa; margin-left: 40px; margin-top: -10px; margin-bottom: 30px;'>for #{account}</h3>"
+      response += "<ul style='width: 90%;'>"
+
+      new_messages.sort_by{|x| x[0]}.each do |m|
+        response += "<li style='margin-bottom: 10px; list-style: none; color: #3485ae; border-bottom: 1px dotted #ccc; padding-bottom: 10px; font-weight: bold;'>"
+        response += m[0]
+        response += ": <strong style='color: #000000; font-weight: normal;'>"
+
+        if configs["use_preview"]
+          response += "<a href='http://"
+          response += configs["bucket"]
+          response += ".s3.amazonaws.com/"
+          response += email_address.gsub("@", "_").gsub(".", "_")
+          response += "_"
+          response += Digest::SHA1.hexdigest(m[1])
+          response += ".html'>#{m[1]}</a>"
+        else
+          response += m[1]
+        end
+
+        response += "</strong> </li>"
+      end
+
+      response += "</ul>"
+      return response
+    end
+
+    def self.generate_previews(new_messages, configs, email_address)
+      AWS::S3::Base.establish_connection!(
+        :access_key_id     => configs["aws_key"],
+        :secret_access_key => configs["aws_secret"]
+      )
+
+      # set file prefix. useful when we have more than one
+      # anotherinbox account running in the same script instance
+      prefix   = email_address.gsub("@", "_").gsub(".", "_")
+      
+      #binding.pry
+      
+      # cleanup the bucket
+      while (AWS::S3::Bucket.objects(configs["bucket"], :prefix => prefix).size > 0) do
+        AWS::S3::Bucket.objects(configs["bucket"]).each do |file|
+            file.delete if file.key.match(prefix)
+        end
+      end
+      
+      new_messages.sort_by{|x| x[0]}.each do |m|
+        filename = Digest::SHA1.hexdigest(m[1])
+        AWS::S3::S3Object.store("#{prefix}_#{filename}.html", m[2].to_s, configs["bucket"], :access => :public_read)
+      end
+    end
+    
+
+    log         = Logger.new(STDOUT)
+    emails      = []
+    aib_domain  = email_address.split("@")[1]
+
+    Gmail.new(email_address, password) do |gmail|
+      gmail.mailbox('[Gmail]/All Mail').emails(:on => (Date.today - 1)).each do |email|
+        next if email.subject.match("Daily Digest for") rescue nil
+        next if "#{email.from[0]['mailbox']}@#{email.from[0]['host']}" == email_address
+        mailbox = email[:to][0].mailbox.downcase rescue "unknown"
+        body    = email.html_part.nil? ? email.body : email.html_part.body
+        emails << [mailbox, email.subject, body]
+      end
+
+      if emails.size > 0
+            if configs["use_preview"]
+              log.info("Generating previews...")
+              self.generate_previews(emails, configs, email_address)
+            end
+
+            log.info("Sending daily digest with #{emails.size} entries..")
+            digest                 = gmail.message
+            digest.to              = target_email
+            digest.subject         = "[#{aib_domain}] Daily Digest for #{(Date.today - 1)}"
+            digest.content_type    = "text/html"
+            digest.body            = self.email_template(emails, aib_domain, configs, email_address)
+            digest.deliver!
+    end
+    end
+    log.info("Process finished")
+  end
+end
