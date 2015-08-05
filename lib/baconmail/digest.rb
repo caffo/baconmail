56dd9d70aacf417579234a6d0f40f279664cab44
Rodrigo Franco <caffeine@gmail.com>
2015-08-04 22:30:56 -0300

    Initial work in anonymizer

--- lib/baconmail/digest.rb
+++ lib/baconmail/digest.rb
@@ -5,26 +5,26 @@ module Baconmail
       log         = Logger.new(STDOUT)
       emails      = []
       aib_domain  = account.username.split("@")[1]
-    
+
       log.info("Account: #{account.username}")
-    
+
       Gmail.new(account.username, account.password) do |gmail|
         gmail.mailbox('[Gmail]/All Mail').emails(:on => (Date.today - 1)).each do |email|
-           
+
           next if email.subject.match("Daily Digest for") rescue nil
           next if "#{email.from[0]['mailbox']}@#{email.from[0]['host']}" == account.username
-            
+
           mailbox = email[:to][0].mailbox.downcase rescue "unknown"
           body    = email.html_part.nil? ? email.body : email.html_part.body
           emails << [mailbox, email.subject, body]
         end
-    
+
         if emails.size > 0
               if configs.use_preview
                 log.info("Generating previews...")
                 self.generate_previews(emails, configs, account.username)
               end
-    
+
               log.info("Sending daily digest with #{emails.size} entries..")
               digest                 = gmail.message
               digest.to              = account.email
@@ -36,7 +36,7 @@ module Baconmail
       end
       log.info("Process finished")
     end
-    
+
     def self.email_template(new_messages, account, configs, email_address)
         # we all know, inline styles sucks. sadly, it's the
         # only way to get them into gmail.
@@ -44,12 +44,12 @@ module Baconmail
         response += "<h1 style='margin-left: 40px; color: #DC6582;'>Bacon Mail for #{(Date.today - 1)}</h1>"
         response += "<h3 style='color: #F59FAC; margin-left: 40px; margin-top: -10px; margin-bottom: 30px;'>for <a href='http://#{account}' style='color: #F59FAC;'>#{account}</a></h3>"
         response += "<ul style='width: 90%;'>"
-    
+
         new_messages.sort_by{|x| x[0]}.each do |m|
           response += "<li style='margin-bottom: 10px; list-style: none; color: #DC6582; border-bottom: 1px dotted #ccc; padding-bottom: 10px; font-weight: bold;'>"
           response += m[0]
           response += ": <strong style='color: #000000; font-weight: normal;'>"
-    
+
           if configs.use_preview
             response += "<a href='http://"
             response += configs.bucket
@@ -61,37 +61,49 @@ module Baconmail
           else
             response += m[1]
           end
-    
+
           response += "</li>"
         end
-    
+
         response += "</ul>"
         return response
     end
-    
+
     def self.generate_previews(new_messages, configs, email_address)
         AWS::S3::Base.establish_connection!(
           :access_key_id     => configs.aws_key,
           :secret_access_key => configs.aws_secret
         )
-    
+
         # set file prefix. useful when we have more than one
         # baconmail account running in the same script instance
         prefix   = email_address.gsub("@", "_").gsub(".", "_")
-          
+
         #binding.pry
-          
+
         # cleanup the bucket
         while (AWS::S3::Bucket.objects(configs.bucket, :prefix => prefix).size > 0) do
           AWS::S3::Bucket.objects(configs.bucket).each do |file|
               file.delete if file.key.match(prefix)
           end
         end
-          
+
         new_messages.sort_by{|x| x[0]}.each do |m|
           filename = ::Digest::SHA1.hexdigest(m[1])
-          AWS::S3::S3Object.store("#{prefix}_#{filename}.html", m[2].to_s, configs.bucket, :access => :public_read)
+          AWS::S3::S3Object.store("#{prefix}_#{filename}.html", anonymize(m[2].to_s, configs), configs.bucket, :access => :public_read)
         end
+    end
+
+    def self.setup_redir
+      content = "<html><head><meta charset='utf-8'><title>redirecting</title><script type='text/javascript'>var queryDict = {};location.search.substr(1).split('&').forEach(function(item) {queryDict[item.split('=')[0]] = item.split('=')[1]});window.location = unescape(queryDict.url);</script></head><body></body></html>"
+      AWS::S3::S3Object.store("redir.html", content, configs.bucket, :access => :public_read)
+    end
+
+    def self.anonymize(message, configs)
+      base_path                        = "http://#{configs.bucket}.s3.amazonaws.com/"
+      doc                              = Nokogiri::HTML(message)
+      doc.css("a").each{ |x| x['href'] = "#{base_path}?url=" + CGI.escape(x['href']) }
+      return doc
     end
   end
 end
