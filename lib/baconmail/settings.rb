8840c6113960fe793ef34643c0dc4b24d6754f2d
Rodrigo Franco <rodrigo@rodrigofranco.com>
2019-04-23 14:39:43 -0300

    Add missing blacklist setting

--- lib/baconmail/settings.rb
+++ lib/baconmail/settings.rb
@@ -6,9 +6,11 @@ module Baconmail
     attr_reader :accounts, :config
   
     def initialize
-      settings = YAML::load(File.open("#{ENV['HOME']}/.baconmail"))
-      @accounts = settings["accounts"].map{ |account| Account.new(account["username"], account["password"], account["email"]) }
-      @config = Config.new(settings["config"]["use_preview"], settings["config"]["bucket"], settings["config"]["aws_key"], settings["config"]["aws_secret"])
+      settings   = YAML::load(File.open("#{ENV['HOME']}/.baconmail"))
+
+      @blacklist = [*settings["blacklist"]]
+      @accounts  = settings["accounts"].map{ |account| Account.new(account["username"], account["password"], account["email"]) }
+      @config    = Config.new(settings["config"]["use_preview"], settings["config"]["bucket"], settings["config"]["aws_key"], settings["config"]["aws_secret"])
     end
   end
 end
